c5a062d94894b20994e2d7264975cce1edc86f77
MercedesReyLara <90905555+MercedesReyLara@users.noreply.github.com>
2024-06-07 02:13:20 +0200

    intento de notificaciones

--- app/src/main/java/com/example/tfg/logIn.kt
+++ app/src/main/java/com/example/tfg/logIn.kt
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageButton
@@ -29,6 +30,8 @@ class logIn : AppCompatActivity() {
         /*Dromimos el main para que se vea el splash*/
         Thread.sleep(1500)
         /*Aplicamos el tema por defecto de mi app*/
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+
         setTheme(R.style.Base_Theme_TFG)
 
         super.onCreate(savedInstanceState)
